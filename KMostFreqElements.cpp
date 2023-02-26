392bd5c6f84a6a00f27f5f56da8c298be883a515
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-02-26 18:48:54 +0530

    next commit

--- KMostFreqElements.cpp
+++ KMostFreqElements.cpp
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/top-k-frequent-elements/
+
+//T.C = O(N*LogN) S.C. = O(N)
 vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> ans;
         unordered_map<int,int> mp;
@@ -10,5 +12,36 @@ vector<int> topKFrequent(vector<int>& nums, int k) {
         sort(vp.rbegin(),vp.rend());
         for(int i=0;i<k;i++)
         ans.push_back(vp[i].second);
+        return ans;
+    }
+
+//T.C = O(K*LogK) S.C = O(N)
+ vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(int x:nums)
+        mp[x]++;
+        int temp=0;
+        auto it = mp.begin();
+        for(it=mp.begin();it!=mp.end() && temp<k;it++)
+        {
+            pq.push({it->second,it->first});
+            temp++;
+        }
+        for(it;it!=mp.end() && temp<nums.size();it++)
+        {
+            if(!pq.empty() && pq.top().first < it->second)
+            {
+               pq.pop();
+               pq.push({it->second,it->first});
+            }
+            temp++;
+        }
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
         return ans;
     }
